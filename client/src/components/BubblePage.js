e6d3368583dc1e6b01f42841d744d2d2f08ec73d
Chris Forrest <redrider_cr@yahoo.com>
2020-07-17 20:37:50 -0400

    bubble page get working colors being displayed

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,6 +8,8 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  
   useEffect(() => {
     axiosWithAuth().get("/colors")
     .then( res => {
