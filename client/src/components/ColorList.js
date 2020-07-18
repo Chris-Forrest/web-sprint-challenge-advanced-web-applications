68c11a68fc71e8ee3d2899e7286a7c50542eb9c3
Chris Forrest <redrider_cr@yahoo.com>
2020-07-17 21:17:12 -0400

    edit colors working

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { useParams } from 'react-router-dom';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -10,6 +11,8 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  /**************** useParams  ********************************************************/
+  const { id } = useParams();
 
   const editColor = color => {
     setEditing(true);
@@ -21,6 +24,20 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${id}`, colorToEdit)
+      .then( res => {
+        console.log("colorlist res data", res.data)
+        const newColors = colors.filter(color => {
+          if(color.id !== id) {
+            return color
+          }
+        })
+        updateColors([...newColors, res.data]);
+        setEditing(false);
+        setColorToEdit(initialColor);
+      })
+      .catch( err => console.log('error editing colorlist ', err))
   };
 
   const deleteColor = color => {
